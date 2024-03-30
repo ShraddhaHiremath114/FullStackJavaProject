a3691b15814eba1d5ab15134230c4d9a27cb4607
vireshh112 <viresh0489@gmail.com>
2024-03-30 09:24:14 +0530

    frontend

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,95 @@
+
+import { useNavigate } from 'react-router-dom'
+import EmployeeService from '../services/EmployeeService';
+import React, { useEffect, useState } from "react";
+
+
+const EmployeeList = () => {
+    const navigate = useNavigate();
+  
+    const [loading, setLoading] = useState(true);
+    const [employees, setEmployees] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const response = await EmployeeService.getEmployees();
+          setEmployees(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+        setLoading(false);
+      };
+      fetchData();
+    }, []);
+    
+    const deleteEmployee = (e,id)=>{
+      e.preventDefault();
+      EmployeeService.deleteEmployee(id)
+      .then((res)=>{
+        if(employees){
+          setEmployees((prev)=>{
+            return prev.filter((employee)=>employee.id !== id);
+          });
+        }
+      });
+    };
+    const editEmployee = (e, id) => {
+      e.preventDefault();
+      navigate(`/editEmployee/${id}`);
+    };
+  
+  return (
+    
+   <div className='container mx-auto my-8'>
+   <div className='h-12'>
+        <button onClick={()=>navigate("/addEmployee")} className='rounded bg-slate-600 text-white px-6 py-2 font-semibold'>Add Employee</button>
+    </div>
+    <div className='flex shadow border-b'>
+        <table className='min-w-full'>
+        <thead className="bg-gray-50">
+            <tr>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                First Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Last Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Email ID
+              </th>
+              <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Actions
+              </th>
+            </tr>
+          </thead>
+               
+            {!loading && (
+            <tbody className='bg-white'>
+                {employees.map((employee)=>(
+                <tr deleteEmployee={deleteEmployee} key={employee.id}>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500'>{employee.firstName}</div></td>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500'>{employee.lastName}</div></td>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500'>{employee.email}</div></td>
+                    <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm '>
+                    <a
+          onClick={(e, id) => editEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">
+          Edit
+        </a>
+                        <a onClick={(e,id)=> deleteEmployee(e,employee.id)} className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Delete</a>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            )}
+        </table>
+
+    </div>
+   </div>
+    
+  );
+};
+
+export default EmployeeList
